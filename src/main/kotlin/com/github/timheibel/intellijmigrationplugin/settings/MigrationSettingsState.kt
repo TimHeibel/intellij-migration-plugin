60bc1b5b1bb44120cc4aeac7e981291c85ae4ccc
Tim Heibel <tim.heibel@uni-ulm.de>
2023-11-13 12:36:35 +0100

    feat: add State for Migration Settings

---
+++ src/main/kotlin/com/github/timheibel/intellijmigrationplugin/settings/MigrationSettingsState.kt
@@ -0,0 +1,28 @@
+package com.github.timheibel.intellijmigrationplugin.settings
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.util.xmlb.XmlSerializerUtil
+import java.awt.Color
+
+@State(name = "org.intellij.sdk.settings.AppSettingsState", storages = [Storage("SettingsMigrationHelper.xml")])
+internal class MigrationSettingsState : PersistentStateComponent<MigrationSettingsState?> {
+    var colorKeywordMapping: Map<String, Color> = emptyMap()
+    var fileTypeCommentMapping: Map<String, String> = emptyMap()
+    var legacyFolderPath: String = ""
+
+    override fun getState(): MigrationSettingsState {
+        return this
+    }
+
+    override fun loadState(state: MigrationSettingsState) {
+        XmlSerializerUtil.copyBean(state, this)
+    }
+
+    companion object {
+        val instance: MigrationSettingsState
+            get() = ApplicationManager.getApplication().getService(MigrationSettingsState::class.java)
+    }
+}
