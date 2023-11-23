a422233b642f6a1510bac398bc6ae74e892f995e
David Blobel <blobel.david@gmail.com>
2023-11-23 23:22:24 +0100

    feat: implemented AnnotationActions Ref.: #20
    Annotation Placement

---
+++ src/main/kotlin/com/github/timheibel/intellijmigrationplugin/actions/annotation/AnnotationAction.kt
@@ -0,0 +1,61 @@
+package com.github.timheibel.intellijmigrationplugin.actions.annotation
+
+import com.github.timheibel.intellijmigrationplugin.annotationModel.AnnotationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.command.WriteCommandAction
+
+
+/**
+ * Abstract class used to create uniform Annotations independent of AnnotationType
+ *
+ * @property annotationType declares the type of Annotation to be set.
+ * @property actionPerformed placement of Annotations
+ */
+abstract class AnnotationAction : AnAction() {
+
+    abstract val annotationType : AnnotationType;
+    override fun actionPerformed(event: AnActionEvent) {
+
+        //Get Required information from the event
+        val project = event.getRequiredData(CommonDataKeys.PROJECT)
+        val editor = event.getRequiredData(CommonDataKeys.EDITOR)
+        val document = editor.document
+        val primaryCaret = editor.caretModel.primaryCaret
+
+        //Get Start and End of current selection
+        val startSelection = primaryCaret.selectionStart
+        val endSelection = primaryCaret.selectionEnd
+        val startSelectionLine = document.getLineNumber(startSelection)
+        val endSelectionLine = document.getLineNumber(endSelection)
+
+        WriteCommandAction.runWriteCommandAction(project) {
+            document.insertString(document.getLineEndOffset(endSelectionLine), "\n//END\n")
+            document.insertString(document.getLineStartOffset(startSelectionLine), "//${annotationType.name}\n")
+        }
+
+        primaryCaret.removeSelection()
+    }
+}
+
+/**
+ * Sets MIGRATED-Annotations
+ * @see AnnotationAction
+ */
+class MIGRATEDAnnotationAction(override val annotationType: AnnotationType = AnnotationType.MIGRATED)
+    : AnnotationAction()
+
+/**
+ * Sets LATER-Annotations
+ * @see AnnotationAction
+ */
+class LATERAnnotationAction(override val annotationType: AnnotationType = AnnotationType.LATER)
+    : AnnotationAction()
+
+/**
+ * Sets UNUSED-Annotations
+ * @see AnnotationAction
+ */
+class UNUSEDAnnotationAction(override val annotationType: AnnotationType = AnnotationType.UNUSED)
+    : AnnotationAction()
