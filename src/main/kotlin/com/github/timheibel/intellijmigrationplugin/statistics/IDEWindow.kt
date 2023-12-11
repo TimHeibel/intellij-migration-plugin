7f2205fb40b284f3368c4c984446279c0d2324eb
sup11 <annika.riepe@uni-ulm.de>
2023-12-11 15:21:35 +0100

    feat: implement fileChooser #34

--- src/main/kotlin/com/github/timheibel/intellijmigrationplugin/statistics/IDEWindow.kt
+++ src/main/kotlin/com/github/timheibel/intellijmigrationplugin/statistics/IDEWindow.kt
@@ -1,6 +1,5 @@
 package com.github.timheibel.intellijmigrationplugin.statistics
 
-import com.github.timheibel.intellijmigrationplugin.MyBundle
 import com.github.timheibel.intellijmigrationplugin.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
@@ -9,31 +8,47 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
+import java.io.File
 import javax.swing.JButton
+import javax.swing.JFileChooser
+
 
 class IDEWindow : ToolWindowFactory {
 
-    init {
-
-    }
-    override fun createToolWindowContent(project: Project, statisticsWindow: ToolWindow){
+    override fun createToolWindowContent(project: Project, statisticsWindow: ToolWindow) {
         val myStatisticsWindow = MyStatisticsWindow(statisticsWindow)
         val content = ContentFactory.getInstance().createContent(myStatisticsWindow.getContent(), null, false)
         statisticsWindow.contentManager.addContent(content)
     }
+
     override fun shouldBeAvailable(project: Project) = true
 
     class MyStatisticsWindow(statisticsWindow: ToolWindow) {
 
+
         private val service = statisticsWindow.project.service<MyProjectService>()
-
         fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel("This is a Button that does nothing")
 
+            var label = JBLabel("This is a Button that does nothing")
+            val label2 = JBLabel("\n")
             add(label)
-            add(JButton("Button")).apply {
+            add(label2)
+            add(JButton("Select File").apply {
+                // button opens a File selector
+                //TODO: multiple files
+                addActionListener {
+                    val fileChooser =  JFileChooser()
+                    fileChooser.showOpenDialog(null)
+                    val response = fileChooser.showSaveDialog(null)
+                    // save filepath
+                    if (response == JFileChooser.APPROVE_OPTION) {
+                       val file = File(fileChooser.selectedFile.absolutePath)
+                        println(file)
+                        label2.text = file.absolutePath
+                    }
 
-            }
+                }
+            })
         }
     }
 }
