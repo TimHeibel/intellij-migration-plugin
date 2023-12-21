66d53b66067addd080d3d7ddcc9d49ee49fc3820
David Blobel <blobel.david@gmail.com>
2023-12-21 18:38:02 +0100

    fix: Annotation where not properly registered
    fixed by using the event-document instead of the actual file

--- src/main/kotlin/intellijmigrationplugin/annotationModel/AnnotationDetection.kt
+++ src/main/kotlin/intellijmigrationplugin/annotationModel/AnnotationDetection.kt
@@ -1,74 +1,70 @@
 package intellijmigrationplugin.annotationModel
 
-import io.ktor.util.*
-import java.io.IOException
-import java.nio.file.Files
-import java.nio.file.Path
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.util.TextRange
 
 class AnnotationDetection {
     companion object {
-        fun detectAnnotationInFile(path: Path) : ArrayList<Pair<AnnotationType, Pair<Int,Int>>> {
+        fun detectAnnotationInFile(document: Document, fileType: String?) : ArrayList<AnnotationSnippet> {
 
-            val outputList = ArrayList<Pair<AnnotationType, Pair<Int,Int>>>()
+            val outputList = ArrayList<AnnotationSnippet>()
 
-            try {
-                val commentType = AnnotationInformation.instance!!.commentTypeMapping[path.extension]
-                    ?: "\\"
-                val regexMigrated = getAnnotationRegex(commentType, AnnotationType.MIGRATED.name)
-                val regexLater    = getAnnotationRegex(commentType, AnnotationType.LATER.name)
-                val regexUnused   = getAnnotationRegex(commentType, AnnotationType.UNUSED.name)
-                val regexEnd      = getAnnotationRegex(commentType, "END")
 
-                val reader = Files.newBufferedReader(path)
-
-                var line = reader.readLine()
-
-                var currAnnotation = AnnotationType.UNMARKED
-                var annotationStartLine = 0
-
-                var lineIndex = 0
-                while(line != null) {
-                    if (!line.startsWith("\\")) {
-                        line = reader.readLine()
-                        lineIndex++
-                        continue
-                    }
-
-                    if(currAnnotation == AnnotationType.UNMARKED) {
-                        when {
-                            regexMigrated.matches(line) -> {
-                                currAnnotation = AnnotationType.MIGRATED
-                                annotationStartLine = lineIndex
-                            }
-                            regexLater.matches(line) -> {
-                                currAnnotation = AnnotationType.LATER
-                                annotationStartLine = lineIndex
-                            }
-                            regexUnused.matches(line) -> {
-                                currAnnotation = AnnotationType.UNUSED
-                                annotationStartLine = lineIndex
-                            }
+            val commentType = AnnotationInformation.instance!!.commentTypeMapping[fileType]
+                ?: "//"
+            val regexMigrated = getAnnotationRegex(commentType, AnnotationType.MIGRATED.name)
+            val regexLater    = getAnnotationRegex(commentType, AnnotationType.LATER.name)
+            val regexUnused   = getAnnotationRegex(commentType, AnnotationType.UNUSED.name)
+            val regexEnd      = getAnnotationRegex(commentType, "END")
+            var line: String
+            var currAnnotation = AnnotationType.UNMARKED
+            var annotationStartLine = 0
+            var lineIndex = 0
+            while(lineIndex < document.lineCount) {
+                line = document
+                    .getText(TextRange(document.getLineStartOffset(lineIndex),
+                        document.getLineEndOffset(lineIndex)))
+                if (!line.startsWith(commentType)) {
+                    lineIndex++
+                    continue
+                }
+                if(currAnnotation == AnnotationType.UNMARKED) {
+                    when {
+                        regexMigrated.matches(line) -> {
+                            currAnnotation = AnnotationType.MIGRATED
+                            annotationStartLine = lineIndex
                         }
-                    } else {
-                        when {
-                            regexMigrated.matches(line) || regexLater.matches(line) || regexUnused.matches(line) -> {
-                                outputList.add(Pair(currAnnotation, Pair(annotationStartLine, --lineIndex)))
-                                currAnnotation = AnnotationType.UNMARKED
-                            }
-
-                            regexEnd.matches(line) -> {
-                                outputList.add(Pair(currAnnotation, Pair(annotationStartLine, lineIndex)))
-                                currAnnotation = AnnotationType.UNMARKED
-                            }
+                        regexLater.matches(line) -> {
+                            currAnnotation = AnnotationType.LATER
+                            annotationStartLine = lineIndex
+                        }
+                        regexUnused.matches(line) -> {
+                            currAnnotation = AnnotationType.UNUSED
+                            annotationStartLine = lineIndex
                         }
                     }
+                } else {
+                    when {
+                        regexMigrated.matches(line) || regexLater.matches(line) || regexUnused.matches(line) -> {
+                            outputList.add(AnnotationSnippet(annotationStartLine, --lineIndex,
+                                false, currAnnotation))
+                            currAnnotation = AnnotationType.UNMARKED
+                        }
+                        regexEnd.matches(line) -> {
+                            outputList.add(AnnotationSnippet(annotationStartLine, lineIndex,
+                                true, currAnnotation))
+                            currAnnotation = AnnotationType.UNMARKED
+                        }
+                    }
+                }
 
                     lineIndex++
-                    line = reader.readLine()
                 }
-            } catch (e : IOException) {
-                return ArrayList()
-            }
+
+                if (currAnnotation != AnnotationType.UNMARKED) {
+                    outputList.add(AnnotationSnippet(annotationStartLine, --lineIndex,
+                        false, currAnnotation))
+                }
 
             return outputList
         }
