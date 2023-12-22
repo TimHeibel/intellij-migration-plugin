234fa19e01a2ad4f8c07628e00ef3ce7dd6a2246
David Blobel <blobel.david@gmail.com>
2023-12-22 19:16:13 +0100

    feat: Read in config-data Ref.: 43
    (lagacyfolderpath, markerColorMapping and commentTypeMapping)

--- src/main/kotlin/intellijmigrationplugin/annotationModel/AnnotationInformation.kt
+++ src/main/kotlin/intellijmigrationplugin/annotationModel/AnnotationInformation.kt
@@ -1,33 +1,59 @@
 package intellijmigrationplugin.annotationModel
 
+import com.intellij.openapi.application.ApplicationManager
+import intellijmigrationplugin.settings.MigrationSettingsState
 import java.awt.Color
 
-class AnnotationInformation {
+class AnnotationInformation private constructor() {
+
+    companion object {
+        var instance: AnnotationInformation? = null
+            private set
+            get() {
+                if (field == null) {
+                    field = AnnotationInformation()
+                }
+                return field!!
+            }
+    }
 
 
     private var instance: AnnotationInformation? = null
-    val annotationFiles: HashMap<String, AnnotationFile> = HashMap()
-    var legacyFolerPath: String = ""
-    val markerColorMapping: HashMap<AnnotationType, Color> = HashMap()
-    val commentTypeMapping: HashMap<String, String> = HashMap()
-
-    private constructor() {
-
-    }
-
-    public fun getInstance(): AnnotationInformation {
-        if (instance == null) {
-            instance = AnnotationInformation()
+    private val settings: MigrationSettingsState
+        get() {
+            return ApplicationManager.getApplication().getService(MigrationSettingsState::class.java)
         }
-        return instance!!
-    }
 
 
-    fun getAnnotationFile(name: String): AnnotationFile {
-        return AnnotationFile()
-    }
+    val legacyFolderPath: String
+        get() {
+            return settings.legacyFolderPath
+        }
 
-    fun computeAnnotationRanges(): HashMap<AnnotationType, Int> {
-        return HashMap()
-    }
+    val markerColorMapping: HashMap<AnnotationType, Color>
+        get() {
+            val colorMapping = settings.keywordColorMapping
+
+            val colorHashMap = HashMap<AnnotationType, Color>()
+            for (pair in colorMapping) {
+                colorHashMap[AnnotationType.valueOf(pair.first)] = pair.second
+            }
+
+            return colorHashMap
+        }
+
+
+    val commentTypeMapping: HashMap<String, String>
+        get() {
+            val commentMapping = settings.fileTypeCommentMapping
+
+            val commentHashMap = HashMap<String, String>()
+
+            for (pair in commentMapping) {
+                commentHashMap[pair.first] = pair.second
+            }
+
+            return commentHashMap
+        }
+
 }
