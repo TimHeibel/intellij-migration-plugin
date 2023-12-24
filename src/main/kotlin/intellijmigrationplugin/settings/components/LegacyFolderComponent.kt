462d927c604440d6c30a09c824a665722e889606
Tim Heibel <tim.heibel@uni-ulm.de>
2023-12-25 00:03:24 +0100

    refactor: legacyfolder

--- src/main/kotlin/intellijmigrationplugin/settings/components/LegacyFolderComponent.kt
+++ src/main/kotlin/intellijmigrationplugin/settings/components/LegacyFolderComponent.kt
@@ -9,9 +9,10 @@ import com.intellij.ui.components.JBLabel
 import com.intellij.util.ui.FormBuilder
 import javax.swing.JComponent
 
-class LegacyFolderComponent (private val project: Project) {
+class LegacyFolderComponent(private val project: Project) {
     private val legacyFolderTextField = TextFieldWithBrowseButton()
     private val descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor()
+
     var legacyFolderPath: String
         get() = legacyFolderTextField.text
         set(value) {
@@ -27,6 +28,7 @@ class LegacyFolderComponent (private val project: Project) {
 
     private fun configureLegacyFolderTextField() {
         legacyFolderTextField.emptyText.setText("Optional")
+
         legacyFolderTextField.addActionListener { _ ->
             val chosenFiles = FileChooser.chooseFiles(descriptor, project, null)
             if (chosenFiles.isNotEmpty() && chosenFiles[0] != null) {
