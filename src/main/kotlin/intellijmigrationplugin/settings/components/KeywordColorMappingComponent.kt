1bc04ed1359a43e0cf05b70a9ab0a6aa6b37a01f
Tim Heibel <tim.heibel@uni-ulm.de>
2023-12-25 03:16:02 +0100

    feat: add cell renderer for color

--- src/main/kotlin/intellijmigrationplugin/settings/components/KeywordColorMappingComponent.kt
+++ src/main/kotlin/intellijmigrationplugin/settings/components/KeywordColorMappingComponent.kt
@@ -4,15 +4,19 @@ import com.intellij.openapi.project.Project
 import com.intellij.ui.ColorPicker
 import com.intellij.ui.ToolbarDecorator
 import com.intellij.ui.table.JBTable
-import java.awt.Color
+import java.awt.*
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
+import javax.swing.Icon
+import javax.swing.JLabel
 import javax.swing.JPanel
+import javax.swing.JTable
+import javax.swing.table.DefaultTableCellRenderer
 import javax.swing.table.DefaultTableModel
 
 class KeywordColorMappingComponent(private val project: Project) {
 
-    var tableModel = DefaultTableModel(arrayOf(arrayOf("", "#ffffff")), arrayOf("Filetype", "Comment Type"))
+    var tableModel = DefaultTableModel(arrayOf(arrayOf("", "#ffffff")), arrayOf("Keyword", "Color"))
     var table = JBTable(tableModel)
 
     fun getComponent(): JPanel {
@@ -29,6 +33,9 @@ class KeywordColorMappingComponent(private val project: Project) {
                 }
             }
         })
+
+        // Set custom renderer for the second column
+        table.columnModel.getColumn(1).cellRenderer = ColorCellRenderer()
 
         return decorator.createPanel()
     }
@@ -70,5 +77,41 @@ class KeywordColorMappingComponent(private val project: Project) {
             tableModel.addRow(arrayOf(pair.first, pair.second))
         }
     }
-}
 
+    private class ColorCellRenderer : DefaultTableCellRenderer() {
+        override fun getTableCellRendererComponent(
+            table: JTable?,
+            value: Any?,
+            isSelected: Boolean,
+            hasFocus: Boolean,
+            row: Int,
+            column: Int
+        ): Component {
+            val hexColor = value as? String ?: ""
+            val color = Color.decode(hexColor)
+            val icon = ColorIcon(color)
+            val label = JLabel(icon)
+            label.toolTipText = hexColor
+            return label
+        }
+    }
+
+    private class ColorIcon(private val color: Color) : Icon {
+        override fun paintIcon(c: Component?, g: Graphics?, x: Int, y: Int) {
+            val g2d = g as Graphics2D
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+
+            // White Border
+            g2d.color = Color.WHITE
+            g2d.fillRoundRect(x, y, iconWidth, iconHeight, 4, 4)
+
+            // Fill Border
+            g2d.color = color
+            g2d.fillRoundRect(x + 2, y + 2, iconWidth - 4, iconHeight - 4, 4, 4)
+        }
+
+        override fun getIconWidth() = 16
+
+        override fun getIconHeight() = 16
+    }
+}
