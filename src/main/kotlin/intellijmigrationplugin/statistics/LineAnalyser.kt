4729dae4f86da6daa0fdd625335ca048191f52db
sup11 <annika.riepe@uni-ulm.de>
2024-01-07 12:51:58 +0100

    Chore: get Keywords from settings #35

--- src/main/kotlin/intellijmigrationplugin/statistics/LineAnalyser.kt
+++ src/main/kotlin/intellijmigrationplugin/statistics/LineAnalyser.kt
@@ -1,6 +1,8 @@
 package intellijmigrationplugin.statistics
 
+import com.intellij.openapi.application.ApplicationManager
 import intellijmigrationplugin.annotationModel.AnnotationType
+import intellijmigrationplugin.settings.MigrationSettingsState
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
@@ -10,32 +12,47 @@ import java.util.regex.Pattern
 
 class LineAnalyser {
 
-    private var fileStatisticMap = initializeEnumMap()
-    private fun initializeEnumMap(): HashMap<AnnotationType, Int> {
-        val enumMap = AnnotationType.values().associateWith { 0 }
-        return HashMap(enumMap) // Explicitly converting to HashMap
-    }
 
+
+    private val settings: MigrationSettingsState
+        get() {
+            return ApplicationManager.getApplication().getService(MigrationSettingsState::class.java)
+        }
+
+    private var keywordsList: MutableList<String> = settings.keywordColorMapping.map { it.first }.toMutableList()
     private val regexPattern = ".*\\S|}"
     private val pattern: Pattern = Pattern.compile(regexPattern)
+    private val fileStatisticMap: MutableMap<String, Int> = mutableMapOf<String,Int>()
+
+    init {
+        // Initialize map with default values
+        keywordsList.forEach { keyword ->
+            fileStatisticMap[keyword] = 0
+        }
+    }
+    private fun resetFileStatisticMap() {
+        fileStatisticMap.clear()
+        keywordsList.forEach { keyword ->
+            fileStatisticMap[keyword] = 0
+        }
+    }
     fun pathToFile(filePath: String) {
         //TODO: read files from Projects and analyse lines
-        fileStatisticMap.clear()
-        fileStatisticMap = initializeEnumMap()
+        resetFileStatisticMap()
         val lOC = countLinesInFile(filePath)
         sortLOCbyLabel(filePath)
 
-        //print statistic
-        val linesMigrated = fileStatisticMap[AnnotationType.MIGRATED] ?: -1
-        val linesLater = fileStatisticMap[AnnotationType.LATER] ?: -1
-        val linesUnused = fileStatisticMap[AnnotationType.UNUSED] ?: -1
+        //print statistic temporär
+        val linesMigrated = fileStatisticMap[keywordsList[0]] ?: -1
+        val linesLater = fileStatisticMap[keywordsList[1]] ?: -1
+        val linesUnused = fileStatisticMap[keywordsList[2]] ?: -1
         val unmarked = lOC - (linesLater  + linesUnused + linesMigrated)
         val percent= 100 - ((unmarked/lOC).toDouble() * 100)
-        println("lOC: MIGRATED: LATER: UNUSED: UNMARKED: \n $lOC $linesMigrated $linesLater $linesUnused $unmarked")
+        println("lOC: $lOC $keywordsList[0] : $linesMigrated , $linesLater , $linesUnused, $unmarked")
         println("$percent % out of 100%")
     }
 
-    /// Counts lines of code including comments and returns the lines as String
+    //TODO: filter out comments and imports
     private fun countLinesInFile(filePath: String): Int {
         try {
             BufferedReader(FileReader(filePath)).use { br ->
@@ -58,30 +75,32 @@ class LineAnalyser {
         return -1
     }
 
-    
+
     private fun sortLOCbyLabel(filePath: String){
         val segments = mutableMapOf<String, Pair<String, Int>>()
         var segmentIndex = 0
 
         try {
             File(filePath).useLines { lines ->
-                var currentSegmentKey: AnnotationType? = null
+                var currentSegmentKey: String? = null
                 val currentSegment = StringBuilder()
 
                 lines.forEach { line ->
                     if (currentSegmentKey == null) {
-                        val foundStartKeyword = AnnotationType.values().find { line.contains(it.name) }
+                        //TODO: not annotation type anymore
+                        val foundStartKeyword : String = keywordsList.find { line.contains(it) }.toString()
 
-                        if (foundStartKeyword != null) {
+                        if (foundStartKeyword != "null") {
                             currentSegmentKey = foundStartKeyword
                             return@forEach
                         }
                     } else if (line.contains("END")) {
-                        val segmentKey = "${currentSegmentKey!!.name}-$segmentIndex"
+                        val segmentKey = "${currentSegmentKey!!}-$segmentIndex"
                         val segmentContent = currentSegment.toString()
 
                         val linesOfCode = countLinesInSegment(segmentContent)
-                        fileStatisticMap[currentSegmentKey!!] = (fileStatisticMap[currentSegmentKey]!! + linesOfCode)
+                        val tmpInt: Int = fileStatisticMap[currentSegmentKey]!! + linesOfCode
+                        fileStatisticMap[currentSegmentKey!!] = tmpInt
 
                         segments[segmentKey] = Pair(segmentContent, linesOfCode)
 
@@ -94,7 +113,7 @@ class LineAnalyser {
                 }
                 // Handle the last segment if it doesn't end with "END"
                 if (currentSegmentKey != null && currentSegment.isNotEmpty()) {
-                    val segmentKey = "${currentSegmentKey!!.name}-$segmentIndex"
+                    val segmentKey = "${currentSegmentKey!!}-$segmentIndex"
                     val segmentContent = currentSegment.toString()
                     val linesOfCode = countLinesInSegment(segmentContent)
                     segments[segmentKey] = Pair(segmentContent, linesOfCode)
