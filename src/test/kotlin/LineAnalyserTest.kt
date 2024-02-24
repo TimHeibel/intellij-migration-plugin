5b59bb2f521ee72c46d5f4cd8bf81e3383147aca
AnnikaLR <annika.riepe@gmail.com>
2024-02-24 10:42:07 +0100

    chore: add Test dependencies and missing module folders #69

---
+++ src/test/kotlin/LineAnalyserTest.kt
@@ -0,0 +1,12 @@
+
+import intellijmigrationplugin.statistics.StatisticInformation
+import org.junit.jupiter.api.Test
+
+class LineAnalyserTest {
+
+    @Test
+    fun initialiseKeywords() {
+        val testClass = StatisticInformation()
+    }
+
+}
