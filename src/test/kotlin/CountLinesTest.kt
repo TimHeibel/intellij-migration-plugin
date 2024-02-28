f03bac108c02da3e05bc6e51ed87d98b53e90f80
AnnikaLR <annika.riepe@gmail.com>
2024-02-28 20:41:27 +0100

    test: add tests for LineAnalyser class #33

---
+++ src/test/kotlin/CountLinesTest.kt
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test
+
+class CountLinesTest {
+
+    //count LOC
+    @Test
+    fun multilineComments(){
+
+    }
+
+    @Test
+    fun comments(){
+
+    }
+    @Test
+    fun keywordDetections(){
+
+    }
+
+    @Test
+    fun wrongKeywords(){
+
+    }
+}
--- src/test/kotlin/LineAnalyserTest.kt
+++ src/test/kotlin/LineAnalyserTest.kt
@@ -4,16 +4,37 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class LineAnalyserTest {
+    var lineAnalyser = LineAnalyser()
 
+
+    //test analiseLines()
     @Test
-    fun initialiseKeywords() {
+    fun keywordsListPhython(){
+
+
+    }
+    @Test
+    fun analysePhythonFile() {
         //for testing assertions
 
     }
 
+
+    //test regex()
+    @Test
+    fun phythonRegex(){
+        val informationArray = arrayOf("import", "#", "\\#", "\"\"\"", "\"\"\"")
+        val result = lineAnalyser.setRegex(informationArray).toRegex().toString()
+        //val expected = Pattern.compile("^(?!\\s*import)(?!\\s*\\#).*[^\\s]\$", 8)
+        val expected = "^(?!\\s*import)(?!\\s*\\#).*[^\\s]\$"
+
+        val resultBool = result.equals(expected)
+        Assertions.assertEquals(true, resultBool)
+    }
+
+    //testing getFileInformation()
     @Test
     fun getFileInformationForPhython(){
-        val lineAnalyser = LineAnalyser()
 
         val pathname = "/home/finnika/Documents/Uni/5 Semester/SE Projekt/intellij-migration-plugin/src/test/resources/testFiles/PhythonTestCode.py"
         val importMapping: HashMap<String, String> = hashMapOf(
